08de6be62c304f9403f58e63599cfabeb1b024b5
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-02-17 11:39:07 -0500

    Update device_module.py

--- device_module.py
+++ device_module.py
@@ -24,33 +24,45 @@ def create_data(data):
     # Manually checking through what was read from JSON file
     attributes = data.keys()
 
-    if ('device_id' in attributes):
-        f.write("device_id: %s\n" % data['device_id'])
-        print("device_id:", data['device_id'])
+    # Validating if JSON file is formatted correctly
+    if ('device_id' in attributes and 'patient_id' in attributes and 'data' in attributes):
 
-    if ('patient_id' in attributes):
-        f.write("patient_id: %s\n" % data['patient_id'])
-        print("patient_id:", data['patient_id'])
+        # Checking if device_id is valid
+        if ('device_id' in attributes):
+            f.write("device_id: %s\n" % data['device_id'])
+            print("device_id:", data['device_id'])
 
-    if ('data' in attributes):
-        if (data['data'].get('temperature')):
-            f.write("temperature: %s %s\n" % (data['data']['temperature']['value'], data['data']['temperature']['unit']))
-            print("temperature:", data['data']['temperature']['value'], data['data']['temperature']['unit'])
-        if (data['data'].get('blood_pressure')):
-            f.write("blood_pressure: %s %s\n" % (data['data']['blood_pressure']['value'], data['data']['blood_pressure']['unit']))
-            print("blood_pressure:", data['data']['blood_pressure']['value'], data['data']['blood_pressure']['unit'])
-        if (data['data'].get('pulse')):
-            f.write("pulse: %s %s\n" % (data['data']['pulse']['value'], data['data']['pulse']['unit']))
-            print("pulse", data['data']['pulse']['value'], data['data']['pulse']['unit'])
-        if (data['data'].get('oxygen_level')):
-            f.write("oxygen_level: %s %s\n" % (data['data']['oxygen_level']['value'], data['data']['oxygen_level']['unit']))
-            print("oxygen_level", data['data']['oxygen_level']['value'], data['data']['oxygen_level']['unit'])
-        if (data['data'].get('weight')):
-            f.write("weight: %s %s\n" % (data['data']['weight']['value'], data['data']['weight']['unit']))
-            print("weight", data['data']['weight']['value'], data['data']['weight']['unit'])
-        if (data['data'].get('blood_sugar_level')):
-            f.write("blood_sugar_level: %s %s\n" % (data['data']['blood_sugar_level']['value'], data['data']['blood_sugar_level']['unit']))
-            print("weight", data['data']['blood_sugar_level']['value'], data['data']['blood_sugar_level']['unit'])
+        # Checking if patient_id is valid
+        if ('patient_id' in attributes):
+            f.write("patient_id: %s\n" % data['patient_id'])
+            print("patient_id:", data['patient_id'])
+
+        # Checking 
+        if ('data' in attributes):
+            if (data['data'].get('temperature')):
+                if (float(data['data']['temperature']['value']) > 0) and (data['data']['temperature']['unit'] == 'F' or data['data']['temperature']['unit'] == 'C'):
+                    f.write("temperature: %s %s\n" % (data['data']['temperature']['value'], data['data']['temperature']['unit']))
+                    print("temperature:", data['data']['temperature']['value'], data['data']['temperature']['unit'])
+            if (data['data'].get('blood_pressure')):
+                if (data['data']['blood_pressure']['value'].find('/') != -1) and data['data']['blood_pressure']['unit'] == 'mmHg':
+                    f.write("blood_pressure: %s %s\n" % (data['data']['blood_pressure']['value'], data['data']['blood_pressure']['unit']))
+                    print("blood_pressure:", data['data']['blood_pressure']['value'], data['data']['blood_pressure']['unit'])
+            if (data['data'].get('pulse')):
+                if (float(data['data']['pulse']['value']) > 0) and data['data']['pulse']['unit'] == 'bpm':
+                    f.write("pulse: %s %s\n" % (data['data']['pulse']['value'], data['data']['pulse']['unit']))
+                    print("pulse:", data['data']['pulse']['value'], data['data']['pulse']['unit'])
+            if (data['data'].get('oxygen_level')):
+                if (float(data['data']['oxygen_level']['value']) > 0) and data['data']['oxygen_level']['unit'] == '%':
+                    f.write("oxygen_level: %s %s\n" % (data['data']['oxygen_level']['value'], data['data']['oxygen_level']['unit']))
+                    print("oxygen_level:", data['data']['oxygen_level']['value'], data['data']['oxygen_level']['unit'])
+            if (data['data'].get('weight')):
+                if (float(data['data']['weight']['value']) > 0) and data['data']['weight']['unit'] == 'lbs':
+                    f.write("weight: %s %s\n" % (data['data']['weight']['value'], data['data']['weight']['unit']))
+                    print("weight:", data['data']['weight']['value'], data['data']['weight']['unit'])
+            if (data['data'].get('blood_sugar_level')):
+                if (float(data['data']['blood_sugar_level']['value']) > 0) and (data['data']['blood_sugar_level']['unit'] == 'mg/dL' or data['data']['blood_sugar_level']['unit'] == 'mmol/L'):
+                    f.write("blood_sugar_level: %s %s\n" % (data['data']['blood_sugar_level']['value'], data['data']['blood_sugar_level']['unit']))
+                    print("blood_sugar_level:", data['data']['blood_sugar_level']['value'], data['data']['blood_sugar_level']['unit'])
 
 
 def main():
